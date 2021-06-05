2677062b33ed93dbada64c07fe029e85a9393d1c
nazmulhyder <nhyder.cse131.uiu@gmail.com>
2021-06-05 20:46:20 +0600

    Configure Logging using Serilog

--- TEST-ARCHITECTURE-DOTNET-CORE-WEB-API/Program.cs
+++ TEST-ARCHITECTURE-DOTNET-CORE-WEB-API/Program.cs
@@ -6,6 +6,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Serilog;
+using Serilog.Events;
 
 namespace TEST_ARCHITECTURE_DOTNET_CORE_WEB_API
 {
@@ -13,11 +15,32 @@ namespace TEST_ARCHITECTURE_DOTNET_CORE_WEB_API
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo.File(
+                    path: "C:\\DotNetCoreLogger\\Test_Architecture\\log_.txt",
+                    outputTemplate: "[{Timestamp:yyyy-MM-dd HH:mm:ss} {Level:u3}] {Message:lj}{NewLine}{Exception}",
+                    rollingInterval: RollingInterval.Day,
+                    restrictedToMinimumLevel: LogEventLevel.Information
+                ).CreateLogger();
+            try
+            {
+                Log.Information("Application Is Starting!");
+                CreateHostBuilder(args).Build().Run();
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Application Failed to Start!");
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
+            
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .UseSerilog()
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
