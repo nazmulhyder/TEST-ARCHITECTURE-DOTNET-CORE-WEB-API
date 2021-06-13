74531ccb0f23fc490c7220e29b46519bce0d5e14
nazmulhyder <nhyder.cse131.uiu@gmail.com>
2021-06-14 00:56:13 +0600

    Repository Pattern Added

---
+++ TEST-ARCHITECTURE-DOTNET-CORE-WEB-API/IRepository/IGenericRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace TEST_ARCHITECTURE_DOTNET_CORE_WEB_API.IRepository
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        Task<IList<T>> GetAll(Expression<Func<T, bool>> expression = null,
+            Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null,
+            List<string> includes = null);
+
+        Task<T> Get(Expression<Func<T, bool>> expression, List<string> includes = null);
+        Task Insert(T entity);
+        Task InsertRange(IEnumerable<T> entities);
+        Task Delete(int id);
+        void DeleteRange(IEnumerable<T> entities);
+        void Update(T entity);
+
+    }
+}
---
+++ TEST-ARCHITECTURE-DOTNET-CORE-WEB-API/Repository/GenericRepository.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using TEST_ARCHITECTURE_DOTNET_CORE_WEB_API.Data;
+using TEST_ARCHITECTURE_DOTNET_CORE_WEB_API.IRepository;
+
+namespace TEST_ARCHITECTURE_DOTNET_CORE_WEB_API.Repository
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        private readonly DatabaseContext _context;
+        private readonly DbSet<T> _db;
+
+        public GenericRepository(DatabaseContext context)
+        {
+            this._context = context;
+            this._db = this._context.Set<T>();
+        }
+        public async Task<IList<T>> GetAll(Expression<Func<T, bool>> expression = null, Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null, List<string> includes = null)
+        {
+            IQueryable<T> query = this._db;
+
+            if (expression != null)
+            {
+                query = query.Where(expression);
+            }
+
+            if (includes != null)
+            {
+                foreach (var includeProperty in includes)
+                {
+                    query.Include(includeProperty);
+                }
+            }
+
+            if (orderBy != null)
+            {
+                query = orderBy(query);
+            }
+
+            return await query.AsNoTracking().ToListAsync();
+        }
+
+        public async Task<T> Get(Expression<Func<T, bool>> expression, List<string> includes = null)
+        {
+            IQueryable<T> query = this._db;
+            if (includes != null)
+            {
+                foreach (var includeProperty in includes)
+                {
+                    query.Include(includeProperty);
+                }
+            }
+
+            return await query.AsNoTracking().FirstOrDefaultAsync(expression);
+        }
+
+        public async Task Insert(T entity)
+        {
+            await this._db.AddAsync(entity);
+        }
+
+        public async Task InsertRange(IEnumerable<T> entities)
+        {
+            await this._db.AddRangeAsync(entities);
+        }
+
+        public async Task Delete(int id)
+        {
+            var entity = await this._db.FindAsync(id);
+            this._db.Remove(entity);
+        }
+
+        public void DeleteRange(IEnumerable<T> entities)
+        {
+            this._db.RemoveRange(entities);
+        }
+
+        public void Update(T entity)
+        {
+            this._db.Attach(entity);
+            this._context.Entry(entity).State = EntityState.Modified;
+        }
+    }
+}
