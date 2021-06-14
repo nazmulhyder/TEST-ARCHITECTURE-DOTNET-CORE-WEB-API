1cb02f908b0d5d420053198c5c95cb2381915114
nazmulhyder <nhyder.cse131.uiu@gmail.com>
2021-06-14 21:37:53 +0600

    Added Unit of Work Pattern

---
+++ TEST-ARCHITECTURE-DOTNET-CORE-WEB-API/IRepository/IUnitOfWork.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TEST_ARCHITECTURE_DOTNET_CORE_WEB_API.Data;
+
+namespace TEST_ARCHITECTURE_DOTNET_CORE_WEB_API.IRepository
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        IGenericRepository<Country> Countries { get; }
+        IGenericRepository<Hotel> Hotels { get; }
+        Task Save();
+    }
+}
---
+++ TEST-ARCHITECTURE-DOTNET-CORE-WEB-API/Repository/UnitOfWork.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TEST_ARCHITECTURE_DOTNET_CORE_WEB_API.Data;
+using TEST_ARCHITECTURE_DOTNET_CORE_WEB_API.IRepository;
+
+namespace TEST_ARCHITECTURE_DOTNET_CORE_WEB_API.Repository
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private IGenericRepository<Country> _countries;
+        private IGenericRepository<Hotel> _hotels;
+        private readonly DatabaseContext _context;
+        public UnitOfWork(DatabaseContext context)
+        {
+            this._context = context;
+        }
+
+        public IGenericRepository<Country> Countries => _countries ?? new GenericRepository<Country>(this._context); 
+        public IGenericRepository<Hotel> Hotels => _hotels ?? new GenericRepository<Hotel>(this._context);
+
+        public void Dispose()
+        {
+            this._context.Dispose();
+            GC.SuppressFinalize(this);
+        }
+        public async Task Save()
+        {
+            await this._context.SaveChangesAsync();
+        }
+    }
+}
